e699fce036125930e955d61d63474f37c9570f97
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2022-08-30 01:16:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://gitbanner.js.org',
+  base: '/gitbanner/',
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
